6a350ebc0418a0f95b827d136ce9cbaa02ed664f
EugeneZem <119622250+EugeneZem@users.noreply.github.com>
2023-09-20 23:18:01 +0300

    Add files via upload

---
+++ 4_widgets/mainwindow.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->gb_radiobox->setTitle("&Радио");
+    ui->rb_button1->setText("&КП 97.2 FM");
+    ui->rb_button2->setText("&Звезда 95.6 FM");
+
+    for (int i = 0; i < 10; ++i)
+    {
+        ui->cb_combo1->addItem("Строка " + QString::number(i + 1));
+    }
+
+    ui->b_button1->setText("&Увеличить");
+
+    ui->pb_progress1->setMinimum(0);
+    ui->pb_progress1->setMaximum(100);
+    ui->pb_progress1->setValue(0);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_b_button1_clicked()
+{
+    if (ui->pb_progress1->value()<ui->pb_progress1->maximum())
+        ui->pb_progress1->setValue(ui->pb_progress1->value()+10);
+    else
+        ui->pb_progress1->setValue(0);
+}
+
+
+void MainWindow::on_pb_progress1_valueChanged(int value)
+{
+    if (ui->pb_progress1->value() == ui->pb_progress1->maximum())
+    {
+        ui->b_button1->setText("&Сброс");
+//        ui->b_button1->setChecked(false);
+    }
+    else
+    {
+        ui->b_button1->setText("&Увеличить");
+    }
+}
+
