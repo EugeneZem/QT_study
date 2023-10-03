ec9ef97978dffd2cf0d1402898ce9a3bf8b05028
EugeneZem <119622250+EugeneZem@users.noreply.github.com>
2023-10-04 00:13:09 +0300

    Add files via upload

---
+++ 5_SlotsSignals/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "stopwatch.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+signals:
+    void SigStart();
+    void SigStop();
+    void SigReset();
+
+private slots:
+    void on_pb_StartStop_clicked();
+    void UpdeteIndicator();
+
+    void on_pb_Round_clicked();
+
+    void on_pb_Clear_clicked();
+
+private:
+    void StartStopwatch();
+    void StopStopwatch();
+    Ui::MainWindow *ui;
+    Stopwatch* stopwatch;
+    int rounds = 0;
+};
+#endif // MAINWINDOW_H
